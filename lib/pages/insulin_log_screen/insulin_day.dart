d6cd921bc265c0286d35ed55b5697a3132698a2c
Janderson <janderson@quimbik.com>
2024-03-24 00:05:33 -0300

    Changing insulin logs font sizes

--- lib/pages/insulin_log_screen/insulin_day.dart
+++ lib/pages/insulin_log_screen/insulin_day.dart
@@ -51,6 +51,7 @@ class InsulinCard extends StatelessWidget {
                   insulinApplication.user!.firstName!,
                   style: const TextStyle(
                     color: Color(0xFF9B6BF3),
+                    fontSize: 16,
                   ),
                 ),
                 Text(
@@ -58,6 +59,7 @@ class InsulinCard extends StatelessWidget {
                       .Hm,
                   style: const TextStyle(
                     color: Color(0xFF888D92),
+                    fontSize: 14,
                   ),
                 ),
               ],
@@ -67,7 +69,7 @@ class InsulinCard extends StatelessWidget {
               "${insulinApplication.insulinUnits!} unidades | ${insulinApplication.glucoseLevel} glicose",
               style: const TextStyle(
                 color: Color(0xFF888D92),
-                fontSize: 12,
+                fontSize: 14,
               ),
             ),
             if (insulinApplication.observations != null &&
@@ -77,7 +79,7 @@ class InsulinCard extends StatelessWidget {
                 "Obs: ${insulinApplication.observations}",
                 style: const TextStyle(
                   color: Color(0xFF888D92),
-                  fontSize: 12,
+                  fontSize: 14,
                 ),
               ),
             ],
