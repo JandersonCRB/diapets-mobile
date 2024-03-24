a7e67388eebb342486faabcee024894ccf763558
Janderson <janderson@quimbik.com>
2024-03-24 00:09:12 -0300

    handling modal filters_modal.dart bottom nav color

--- lib/components/system_ui_customization.dart
+++ lib/components/system_ui_customization.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class SystemUiCustomization extends StatelessWidget {
+  final Color? bottomNavigationColor;
   final Widget child;
 
   const SystemUiCustomization({
     super.key,
     required this.child,
+    this.bottomNavigationColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: SystemUiOverlayStyle(
-        systemNavigationBarColor: Theme.of(context).canvasColor,
+        systemNavigationBarColor:
+            bottomNavigationColor ?? Theme.of(context).canvasColor,
       ),
       child: child,
     );
--- lib/pages/insulin_log_screen/filters_modal.dart
+++ lib/pages/insulin_log_screen/filters_modal.dart
@@ -1,3 +1,4 @@
+import 'package:diapets_mobile/components/system_ui_customization.dart';
 import 'package:flutter/material.dart';
 
 class FiltersModal extends StatelessWidget {
@@ -5,49 +6,52 @@ class FiltersModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        decoration: const BoxDecoration(
-          color: Color(0xFF2E3641),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(24),
-            topRight: Radius.circular(24),
-          ),
-        ),
-        child: Column(
-          children: [
-            const SizedBox(height: 24),
-            Container(
-              width: 40,
-              height: 4,
-              decoration: BoxDecoration(
-                color: const Color(0xFF494F59),
-                borderRadius: BorderRadius.circular(2),
-              ),
+    return SystemUiCustomization(
+      bottomNavigationColor: const Color(0xFF2E3641),
+      child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: const BoxDecoration(
+            color: Color(0xFF2E3641),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24),
+              topRight: Radius.circular(24),
             ),
-            const SizedBox(height: 24),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Text(
-                'Filtros',
-                style: TextStyle(
-                  color: Color(0xFF9B6BF3),
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(height: 24),
+              Container(
+                width: 40,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF494F59),
+                  borderRadius: BorderRadius.circular(2),
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Em breve...',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
+              const SizedBox(height: 24),
+              const Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  'Filtros',
+                  style: TextStyle(
+                    color: Color(0xFF9B6BF3),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-          ],
-        ));
+              const SizedBox(height: 16),
+              const Text(
+                'Em breve...',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
+          )),
+    );
   }
 }
